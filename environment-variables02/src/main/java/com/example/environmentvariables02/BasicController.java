641774d2ea64ec6c0d67986bb3903f50631852b8
Giulia9999 <120981976+Giulia9999@users.noreply.github.com>
2023-05-14 12:39:31 +0200

    Add files via upload

---
+++ environment-variables02/src/main/java/com/example/environmentvariables02/BasicController.java
@@ -0,0 +1,18 @@
+package com.example.environmentvariables02;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class BasicController {
+    @Autowired
+    Environment environment;
+    @GetMapping
+    public String getEnv(){
+        return environment.getProperty("welcomeMSG");
+    }
+}
